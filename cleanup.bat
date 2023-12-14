a3aa61c648fd9bad569dae547056fdc252f70ac0
semapheur <danfyll@hotmail.com>
2023-12-14 13:06:20 +0100

    Fixup

--- cleanup.bat
+++ cleanup.bat
@@ -1,5 +1,5 @@
 @echo off
 
-for /f "tokens=*" %%f in ('dir /b /a-d *.aug *.log *.pdf *.synctex.gz') do (
+for /f "tokens=*" %%f in ('dir /b /a-d *.aux *.log *.pdf *.synctex.gz') do (
   del /f /q "%%f"
 )
